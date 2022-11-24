dbc4232c89a11febc3540a335dca438f8365487b
Nicolas R <rudasn@gmail.com>
2022-11-24 20:11:46 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,52 +10,46 @@ WG_CONF_BK="${WG_CONF}.bk"
 WG_CONF_URL="${WG_CONF_URL:-}"
 WG_CONF_AUTH_TOKEN="${WG_CONF_AUTH_TOKEN:-}"
 
+WG_SLEEP_MIN=${WG_SLEEP_MIN:-45}
+WG_SLEEP_MAX=${WG_SLEEP_MAX:-180}
+
+WG_DEBUG=${WG_DEBUG:-0}
+
 wg_up () {
     echo "wireguard starting.."
 
     wg-quick up $WG_CONF
 
-    if [ $? -eq 0 ]; then
-        wg_backup
-    else
+    # Try restoring from a backup in case of error.
+    if [ $? -ne 0 ]; then
         wg_restore
 
         if [ $? -eq 0 ]; then
             wg_up
         else
             echo "wireguard: unable to start"
+
             if [[ ! "$WG_CONF_URL" ]]; then
                 echo "wireguard exiting with error"
                 exit 1;
             fi
         fi
     fi
+
+    return 0
 }
 
 wg_down () {
+    if [[ ! $(wg show | grep $WG_INAME) ]]; then
+        return 1
+    fi
+
     echo ""
     echo "wireguard stopping.."
 
     wg-quick down $WG_CONF
-}
 
-wg_sync () {
-    echo "wireguard syncing.."
-
-    wg syncconf $WG_INAME <(wg-quick strip $WG_INAME)
-
-    if [ $? -eq 0 ]; then
-        wg_backup
-    else
-        wg_restore
-
-        if [ $? -eq 0 ]; then
-            wg_sync
-        else
-            echo "wireguard: unable to sync"
-            exit 1;
-        fi
-    fi
+    return $?
 }
 
 wg_backup() {
@@ -81,9 +75,20 @@ wg_restore () {
 }
 
 finish () {
+    echo "Wireguard exiting"
+
     wg_down
 
     exit 0
+}
+
+
+rnd() {
+    # Generate a random number for the sleep duration,
+    # to avoid having multiple endpoints pinging the remote
+    # at the exact same time in case of automated deployments.
+
+    echo $(($WG_SLEEP_MIN + $RANDOM % ($WG_SLEEP_MAX - $WG_SLEEP_MIN)))
 }
 
 # Try to exit cleanly
@@ -107,13 +112,15 @@ elif [[ ! "$WG_CONF_URL" ]]; then
 fi
 
 if [[ "$WG_CONF_URL" ]]; then
-    echo "Wireguard listening for config changes"
+
+    sleep_duration_s=$(rnd)
+
+    echo "Wireguard waiting for config changes in ${sleep_duration_s} seconds"
 
     while true; do
-        # generate a random number for the sleep duration
-        # to avoid having multiple endpoints pinging the remote
-        # at the same time.
-        sleep_duration_s=$((1 + $RANDOM % 120))
+        # Sleep now and set next sleep duration
+        sleep $sleep_duration_s
+        sleep_duration_s=$(rnd)
 
         curl_auth_header=''
         if [[ "$WG_CONF_AUTH_TOKEN" ]]; then
@@ -131,32 +138,38 @@ if [[ "$WG_CONF_URL" ]]; then
                 $WG_CONF_URL
         )
 
+        # Try again in case the request failed
         if [ $? -ne 0 ] || [[ $response_http_code != "200" ]]; then
             echo ""
-            echo "Wireguard unable to retrieve config from remote"
-            echo "Server responded with HTTP Code $response_http_code"
-            echo "Retrying in $sleep_duration_s seconds.."
+            echo "Wireguard unable to retrieve config from $WG_CONF_URL."
+            echo "Server responded with HTTP code: $response_http_code"
 
             # Clean up temporary file
             if [ -f $WG_CONF_TMP ]; then
+                cat $WG_CONF_TMP
                 rm $WG_CONF_TMP
             fi
 
-            sleep $sleep_duration_s
+            echo ""
+            echo "Retrying in ${sleep_duration_s} seconds.."
 
             continue
         fi
 
+        # Generate and compare md5 hashes for the current and new config files.
         current_md5=""
         if [ -f $WG_CONF ]; then
             current_md5=$(md5sum $WG_CONF | awk '{print $1}')
         fi
-
         new_md5=$(md5sum "$WG_CONF_TMP" | awk '{print $1}')
 
         if [[ $current_md5 != $new_md5 ]]; then
             echo ""
             echo "Wireguard new config received"
+
+            # Create a backup of our existing config to revert to
+            # in case of errors when loading the new config.
+            wg_backup
 
             # Bring down the existing wireguard interface
             # so as to trigger existing PostDown commands.
@@ -169,24 +182,21 @@ if [[ "$WG_CONF_URL" ]]; then
             # in case the AllowedIPs have changed between configs -
             # apparently these are only applied when the interface is started,
             # not when it is updated.
-            if [[ $(wg show | grep $WG_INAME) ]]; then
-                wg_down
-            fi
+            wg_down
 
-            wg_backup
-
+            # Move temporary file to expected destination
+            # and start wireguard.
             mv $WG_CONF_TMP $WG_CONF
 
             wg_up
-
-            echo ""
-            echo "Next check for config update in $sleep_duration_s seconds"
         else
             # Clean up temporary file
             rm $WG_CONF_TMP
         fi
 
-        sleep $sleep_duration_s
+        if [[ $WG_DEBUG ]]; then
+            echo "Wireguard auto-conf next check in ${sleep_duration_s} seconds"
+        fi
     done
 
     echo ""
